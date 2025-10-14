29e3d07da179bbef4652ccbf7273e9a38e4f93be
Kevin Desktop <kevinluislima10@hotmail.com>
2025-10-13 21:49:09 -0300

    implementando tela de exercicios

---
+++ src/main/java/br/upe/ui/telas/controllers/ExercicioViewController.java
@@ -0,0 +1,31 @@
+package br.upe.ui.telas.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class ExercicioViewController {
+
+    @FXML
+    private Button BCadastrarEX;
+
+    @FXML
+    private Button BDetalhesEX;
+
+    @FXML
+    private Button BEditarEX;
+
+    @FXML
+    private Button BExcluirEX;
+
+    @FXML
+    private Button BListarEX;
+
+    @FXML
+    private ImageView IFechar;
+
+    @FXML
+    private ImageView IMenu;
+
+}
+
