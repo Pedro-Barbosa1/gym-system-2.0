4a5151e3418e31c0120fdc74d7f657729a3c96be
unknown <henriquesantos8960@gmail.com>
2025-10-16 13:17:10 -0300

    feature: admin view controller

---
+++ src/main/java/br/upe/ui/telas/controllers/AdministradorViewController.java
@@ -0,0 +1,30 @@
+package br.upe.ui.telas.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class AdministradorViewController {
+
+    @FXML
+    private ImageView LogoDoApp;
+
+    @FXML
+    private Button BSairDoApp;
+
+    @FXML
+    private Button BListarUsuarios;
+    
+    @FXML
+    private Button BPromoverUsuario;
+
+    @FXML
+    private Button BRebaixarUsuario;
+
+    @FXML
+    private Button BRemoverUsuario;
+
+    @FXML
+    private ImageView IFavoritos;
+
+}
